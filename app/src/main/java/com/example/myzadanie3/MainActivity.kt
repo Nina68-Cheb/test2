c0364c7bd2b262f3d51ac777c390abadda3b60b1
Nina68-Cheb <113776469+Nina68-Cheb@users.noreply.github.com>
2022-09-22 19:12:59 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myzadanie3/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myzadanie3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+fun main(){
+    val cifra = mutableListOf<Int> ()
+    val znachenie = mutableListOf(1,2,3,4,5)
+    for (i In cifra) {
+        znachenie.add(I)
+    }
+    println("$znachenie }")
+}
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
